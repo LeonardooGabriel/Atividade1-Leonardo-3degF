e46cdcc7b3f1b53efe7e56e7ec6eef68164a3d7b
Leonardo Gabriel <126363100+LeonardooGabriel@users.noreply.github.com>
2024-08-05 22:52:29 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const meuNome = "Leonardo Gabriel";
+const idade = "18";
+const altura = "1.74";
+const cidade = "São josé Dos PInhais";
+const massakg = "65kg";
+
+console.log(meuNome);
+console.log(idade);
+console.log(altura);
+console.log(cidade);
+console.log(massakg);
+
+
+   
